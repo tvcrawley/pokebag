2afb0e4cf1574449ec8e3f7c06714cb6fd6dfdf2
tvlangley <chibia2@gmail.com>
2018-01-31 00:04:18 -0500

    Correct code alignment
    Fix code spacing

--- src/components/backpack/Backpack.js
+++ src/components/backpack/Backpack.js
@@ -37,97 +37,95 @@ class Backpack extends Component {
         (result) => {
           console.log(result)
           // console.log('Charmander?: ', result.data.pokemon_entries[3].pokemon_species.name)
-        this.setState({
-          isLoaded: true,
-          pokemon: result.data.pokemon_entries
-        })
-      },
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        })
-      }
-    )
+          this.setState({
+            isLoaded: true,
+            pokemon: result.data.pokemon_entries
+          })
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
   }
 
   handlePokemonClick(index){
     console.log('pokemon added')
-  const backpackCopy = this.state.backpack.slice()
-  backpackCopy.push(this.state.pokemon[index])
-  this.setState({
-    backpack: backpackCopy
-  })
-  console.log(this.state.backpack)
-}
+    const backpackCopy = this.state.backpack.slice()
+    backpackCopy.push(this.state.pokemon[index])
+    this.setState({
+      backpack: backpackCopy
+    })
+    console.log(this.state.backpack)
+  }
 
-handleItemClick(index){
-  console.log('item added')
-  const backpackCopy = this.state.backpack.slice()
-  backpackCopy.push(this.state.items[index])
-  this.setState({
-    backpack: backpackCopy
-  })
-  console.log(this.state.backpack)
-}
+  handleItemClick(index){
+    console.log('item added')
+    const backpackCopy = this.state.backpack.slice()
+    backpackCopy.push(this.state.items[index])
+    this.setState({
+      backpack: backpackCopy
+    })
+    console.log(this.state.backpack)
+  }
 
-handleDeleteFromBackpack(index) {
-  const backpackCopy = this.state.backpack.slice()
-  backpackCopy.splice(index, 1)
-  this.setState({
-    backpack: backpackCopy
-  })
-  console.log(this.state.backpack)
-}
+  handleDeleteFromBackpack(index) {
+    const backpackCopy = this.state.backpack.slice()
+    backpackCopy.splice(index, 1)
+    this.setState({
+      backpack: backpackCopy
+    })
+    console.log(this.state.backpack)
+  }
 
-handleBackpackDetailClick(index) {
-  const backpackCopy = this.state.backpack.slice()
-  backpackCopy[index] = Object.assign({}, backpackCopy[index])
-  backpackCopy[index].showDetails = !backpackCopy[index].showDetails
-  console.log(backpackCopy[index].showDetails)
-  this.setState({backpack: backpackCopy})
-}
+  handleBackpackDetailClick(index) {
+    const backpackCopy = this.state.backpack.slice()
+    backpackCopy[index] = Object.assign({}, backpackCopy[index])
+    backpackCopy[index].showDetails = !backpackCopy[index].showDetails
+    console.log(backpackCopy[index].showDetails)
+    this.setState({backpack: backpackCopy})
+  }
 
   render() {
     const details = ((data) => {
       if(!data.showDetails) {
         return null
-      }
-      else {
+      } else {
         return <p>Type: {data.type}</p>
       }
     })
 
     const backpackList = this.state.backpack.map((data, index) =>
       <li key={index}>
-        <span  onClick={() => this.handleBackpackDetailClick(index)}>{data.pokemon_species.name}</span>
+        <span onClick={() => this.handleBackpackDetailClick(index)}>{data.pokemon_species.name}</span>
         <span onClick={() => this.handleDeleteFromBackpack(index)}>X</span>
         {details(data)}
       </li>
     )
 
-
     const { error, isLoaded, pokemon} = this.state
-    if(error) {
-      return <div>Error: {error.message}</div>
-    } else if(!isLoaded) {
-      return <div>Loading...</div>
-    } else {
-      return (
-        <div className="Backpack">
-          <h2>Backpack</h2>
-          {backpackList}
-        <PokemonList
-          pokemon={this.state.pokemon}
-          onPokemonClick={this.handlePokemonClick}
-        />
-        <ItemList
-          items={this.state.items}
-          onItemClick={this.handleItemClick}
-        />
-      </div>
-    );
-  }
+      if(error) {
+        return <div>Error: {error.message}</div>
+      } else if(!isLoaded) {
+        return <div>Loading...</div>
+      } else {
+        return (
+          <div className="Backpack">
+            <h2>Backpack</h2>
+            {backpackList}
+            <PokemonList
+              pokemon={this.state.pokemon}
+              onPokemonClick={this.handlePokemonClick}
+            />
+            <ItemList
+              items={this.state.items}
+              onItemClick={this.handleItemClick}
+            />
+          </div>
+        );
+      }
   }
 }
 
